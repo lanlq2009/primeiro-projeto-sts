e4ac2b77b050d8d11933fac47c8f3afa7f3d01fc
Allanlq <lanlq2009@gmail.com>
2020-06-30 16:44:52 -0300

    Primeiro  commit

---
+++ src/test/java/com/primeiro/teste/api/MeuPrimeiroTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primeiro.teste.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPrimeiroTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
